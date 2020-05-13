aa49b7686131498834e8fd49399413af0fa8dd9a
sridhar02 <kattasridhar02@gmail.com>
2020-05-13 14:47:51 +0530

    imported components to main component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import Sidebar from "./components/sidebar";
-import axios from "axios";
+import ListOfVehicles from "./components/ListOfVehicles";
+import VehicleDetails from "./components/Vehicle";
+import { makeStyles } from "@material-ui/core";
+
+const useAppStyles = makeStyles({
+  app: {
+    display: "flex",
+    // flex: 1,
+    overflowY: "none",
+  },
+});
 
 function App() {
-  const [vehciles, setVehciles] = useState([]);
-
-  const fetchVehciles = () => {
-    axios
-      .get("http://localhost:3000/vehicles")
-      .then((res) => setVehciles(res.data));
-  };
-
-  useEffect(() => {
-    fetchVehciles();
-  }, []);
-  console.log(vehciles);
+  const classes = useAppStyles();
 
   return (
-    <div className="App">
-     <Sidebar />
+    <div className={classes.app}>
+      <div style={{ width: "50%" }}>
+        <Sidebar />
+        <ListOfVehicles />
+      </div>
+      <div style={{ border: "1px solid black", width: "50%" }}>
+        <VehicleDetails />
+      </div>
     </div>
   );
 }
