8ccfe906f9b94a9d613d7a69d7e063b56129c074
sridhar02 <kattasridhar02@gmail.com>
2020-05-13 14:48:42 +0530

    List of vehicles  in the component

--- src/components/ListOfVehicles.js
+++ src/components/ListOfVehicles.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import InfinteScroll from "react-infinite-scroll-component";
 
-import Vehicle from "./Vehicle";
+import { Typography, makeStyles, Button } from "@material-ui/core";
+import { getVehiclesDetails, selectVehicle } from "../actions/index";
 
-export default function ListOfVehicles() {
-  return <div></div>;
+function Vehicle({ vehicle }) {
+  const dispatch = useDispatch();
+
+  const selectVehicl = () => {
+    const payload = {
+      id: vehicle.id,
+      name: vehicle.name,
+      year: vehicle.year,
+      make: vehicle.make,
+    };
+
+    dispatch(selectVehicle(payload));
+  };
+
+  return (
+    <div>
+      <Button onClick={() => selectVehicl()}>
+        {vehicle.id} {vehicle.name}
+      </Button>
+    </div>
+  );
 }
+
+const useListOfVehiclesStyles = makeStyles({
+  container: {
+    // border: "1px solid black",
+    borderRight: 0,
+    overflowY: "auto",
+    // margin: "20px",
+    // padding: "8px",
+  },
+});
+
+function ListOfVehicles() {
+  const [count, setCount] = useState(50);
+  const vehicles = useSelector((state) => state.vehicles);
+  const dispatch = useDispatch();
+  const classes = useListOfVehiclesStyles();
+
+  const fetchVehicles = () => {
+    setCount(count + 50);
+    dispatch(getVehiclesDetails(count + 50));
+  };
+
+  useEffect(() => {
+    dispatch(getVehiclesDetails(count));
+  }, [dispatch]);
+
+  return (
+    <div className={classes.container}>
+      {/* <Typography variant="h4">Vehicle Details</Typography> */}
+      {vehicles.vehicles && (
+        <InfinteScroll
+          dataLength={vehicles.vehicles.length}
+          next={fetchVehicles}
+          hasMore={true}
+          loader={<h4>loading..</h4>}
+        >
+          {vehicles.vehicles.map((vehicle) => (
+            <Vehicle vehicle={vehicle} key={vehicle.id} />
+          ))}
+        </InfinteScroll>
+      )}
+    </div>
+  );
+}
+
+export default ListOfVehicles;
