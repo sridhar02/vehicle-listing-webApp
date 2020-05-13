dc6fb992a67599262b94355b58b5ff2b2225a14a
sridhar02 <kattasridhar02@gmail.com>
2020-05-13 14:49:34 +0530

    vehicle component updated

--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -1,14 +1,47 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
 
-export default function Vehicle({ vehicle }) {
+import { useSelector } from "react-redux";
+import { Typography, makeStyles } from "@material-ui/core";
+
+const useVehicleDetailsStyles = makeStyles({
+  container: {
+    marginTop: "30px",
+  },
+  title: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  vehicle: {
+    // border: "1px solid black",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    // width: "30%",
+    marginTop: "40px",
+  },
+});
+
+export default function VehicleDetails() {
+  const classes = useVehicleDetailsStyles();
+  const vehicles = useSelector((state) => state.vehicles);
+  // console.log(vehicles.vehicle);
+
   return (
-    <div>
-      <Typography variant="h4">Vehicle Details</Typography>
-      <Typography variant="h6"> {vehicle.MakeId}</Typography>
-      <Typography variant="h6"> {vehicle.MakeName}</Typography>
-      <Typography variant="h6"> {vehicle.VehicleTypeId}</Typography>
-      <Typography variant="h6"> {vehicle.VehicleTypeName}</Typography>
+    <div className={classes.container}>
+      <Typography className={classes.title} variant="h4">
+        Vechile Details Panel
+      </Typography>
+      <div>
+        {vehicles.vehicle && (
+          <div>
+            <Typography variant="h6">Id:{vehicles.vehicle.id}</Typography>
+            <Typography variant="h6">Name : {vehicles.vehicle.name}</Typography>
+            <Typography variant="h6"> Year:{vehicles.vehicle.year}</Typography>
+            <Typography variant="h6"> Make: {vehicles.vehicle.make}</Typography>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
