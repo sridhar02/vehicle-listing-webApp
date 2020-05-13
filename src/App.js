09f952e47a521e62e6b51492e5a3c3802c08b3bc
sridhar02 <kattasridhar02@gmail.com>
2020-05-13 08:50:34 +0530

    import axios for fetch request and sidebar component to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import Sidebar from "./components/sidebar";
+import axios from "axios";
 
 function App() {
+  const [vehciles, setVehciles] = useState([]);
+
+  const fetchVehciles = () => {
+    axios
+      .get("http://localhost:3000/vehicles")
+      .then((res) => setVehciles(res.data));
+  };
+
+  useEffect(() => {
+    fetchVehciles();
+  }, []);
+  console.log(vehciles);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Sidebar />
     </div>
   );
 }
