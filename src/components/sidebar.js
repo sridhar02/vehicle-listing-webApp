5078dd34cfec556bca55cf48dc6cffc9dbfb3c52
sridhar02 <kattasridhar02@gmail.com>
2020-05-13 14:49:18 +0530

    Sidebar component modification

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 import AddIcon from "@material-ui/icons/Add";
-import { makeStyles, Typography } from "@material-ui/core";
+import { makeStyles, Typography, Button } from "@material-ui/core";
 import FilterListIcon from "@material-ui/icons/FilterList";
 
 const useSidebarStyles = makeStyles({
   container: {
     display: "flex",
-    // justifyContent: "space-between",
+    justifyContent: "space-between",
   },
 });
 
@@ -15,9 +15,15 @@ export default function Sidebar() {
   const classes = useSidebarStyles();
   return (
     <div className={classes.container}>
-      <Typography>Vehicles</Typography>
-      <FilterListIcon />
-      <AddIcon />
+      <Typography variant="h3" >Vehicles</Typography>
+      <div>
+        <Button>
+          <FilterListIcon />
+        </Button>
+        <Button>
+          <AddIcon />
+        </Button>
+      </div>
     </div>
   );
 }
